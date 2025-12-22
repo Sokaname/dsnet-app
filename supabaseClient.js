e3afb521fa8f35b365cd3052a8086ac24b1b95bf
rafa2020carv <rafa2020carv@gmail.com>
2025-12-22 01:17:02 +0000

    refactor: Use environment variables for Supabase keys

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Sua URL correta
-const supabaseUrl = 'https://bxgpfvzfhjmyljqpyrbh.supabase.co'
-
-// A Chave que começa com eyJh... (Cole a sua aqui dentro)
-const supabaseKey = 'sb_publishable_YtwJXizwMUGnLhG0MDxXmQ_CzWEWwLl'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
